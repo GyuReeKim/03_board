8c365f3c524661b7ebdd5d587b26b790120fd9ec
GyuReeKim <starkim96@naver.com>
2019-08-07 10:29:11 +0900

    model 선언

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# 데이터를 저장하는 구조 만들기
+class Todo(models.Model):
+    # 제목
+    title = models.CharField(max_length=50)
+    # 상세내용
+    content = models.CharField(max_length=200)
+    # 마감일
+    due_date = models.DateField()
