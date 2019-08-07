b1ee8c73b7a7225ca4f65d3234036fc5bc9656c7
GyuReeKim <starkim96@naver.com>
2019-08-07 11:37:46 +0900

    detail

--- todos/urls.py
+++ todos/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('new/', views.new), # form을 보여준다.
     path('create/', views.create),
     # Read
+    # 경로가 지정되있지 않으면 연결해준다.
     path('', views.index),
+    # 지정된 위치에 들어오는 숫자를 todo_id라고 한다.
+    path('<int:todo_id>/', views.detail),
 ]
